6f902abe733d3550fba30d746b5c024613dc0e5b
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-25 16:08:12 +0200

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const ref = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+let timerIntervalChangeColor = null;
+const LOCALSTR_KEY = 'colorBody';
 
+let saverColorBody = localStorage.getItem(LOCALSTR_KEY);
+ref.body.style.backgroundColor = saverColorBody;
+
+ref.btnStop.disabled = true;
+
+ref.btnStart.addEventListener('click', onClickBtnStart);
+ref.btnStop.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart() {
+  timerIntervalChangeColor = setInterval(changeColorBody, 1000);
+  ref.btnStart.classList.toggle('hover');
+  ref.btnStop.classList.toggle('hover');
+  ref.btnStart.disabled = true;
+  ref.btnStop.disabled = false;
+}
+
+function onClickBtnStop() {
+  ref.btnStop.classList.toggle('hover');
+  ref.btnStart.classList.toggle('hover');
+  ref.btnStart.disabled = false;
+  ref.btnStop.disabled = true;
+  clearInterval(timerIntervalChangeColor);
+}
+
+function changeColorBody() {
+  const colorBody = getRandomHexColor();
+  ref.body.style.backgroundColor = colorBody;
+  localStorage.setItem(LOCALSTR_KEY, colorBody);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
