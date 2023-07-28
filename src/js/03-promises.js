3d7f6843743332d7c828cb86eff1851bbfb8d85f
BishopNik <128685383+BishopNik@users.noreply.github.com>
2023-07-28 16:21:52 +0200

    task-3 begin

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,23 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+// Notify.info('Hello!');
+
+console.log('sdjhgfjsdh');
+
+// const refForm = document.querySelector('.form');
+// console.log('sdjhgfjsdh');
+// refForm.addEventListener('submit', onSubmit);
+
+// function onSubmit(evt) {
+//   evt.preventDefault();
+//   console.log(evt);
+// }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
